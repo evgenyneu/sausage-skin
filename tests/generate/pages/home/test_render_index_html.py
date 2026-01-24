637355a24d6fe8020f37b78ec0bb5b65d0195125
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2026-01-24 19:53:46 +0000

    Refactor

--- tests/generate/pages/home/test_render_index_html.py
+++
@@ -1,9 +0,0 @@
-from src.generate.pages.home import render_index_html
-
-
-def test_render_index_html_replaces_placeholder() -> None:
-    layout_html = "<html><body>{{ body }}</body></html>"
-
-    result = render_index_html(layout_html=layout_html, body_html="<div>Hello World</div>")
-
-    assert result == "<html><body><div>Hello World</div></body></html>"
