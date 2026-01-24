c1228dfa6db7ddf395aad2a5100e61de6f8b498e
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2026-01-24 07:50:19 +0000

    Reorganize tests location

---
+++ tests/generate/process/main/test_process_single_track.py
@@ -0,0 +1,25 @@
+from datetime import date as date_type
+from pathlib import Path
+
+from src.generate.discover_tracks import TrackInfo
+from src.generate.process.main import process_single_track
+from src.generate.yaml.main import TrackMetadata
+
+
+def test_process_single_track_no_error(tmp_path: Path) -> None:
+    track_dir = tmp_path / "track"
+    track_dir.mkdir()
+    track_yml_path = track_dir / "track.yml"
+
+    track = TrackInfo(
+        track_dir=track_dir,
+        track_yml_path=track_yml_path,
+        date=date_type(2024, 1, 1),
+        metadata=TrackMetadata(
+            title="test",
+            url="test",
+            description="test description",
+        ),
+    )
+
+    process_single_track(track=track)
